a6bd1ecb72c2d66ff48e8141fde3bdd704bb5afe
Michael Privat <mprivat@majorspot.com>
2020-11-07 12:19:53 -0500

    fix: added conversion of anchor links to lowercase

--- anchors.py
+++ anchors.py
@@ -1,3 +1,9 @@
 import sys
 import re
-print(re.sub(r'\]\((?!#|http)', r'](#', sys.stdin.read(), flags=re.M))
+result = re.sub(r'\]\((?!#|http)', r'](#', sys.stdin.read(), flags=re.M)
+
+def upper_repl(match):
+     return '](#' + match.group(1).lower()
+
+result = re.sub(r'\]\(#(.*)\)', upper_repl, result, flags=re.M)
+print(result)
